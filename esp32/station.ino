2486482c3345537d6d5066dd5016d8d11888031d
astrop15 <Myriam.gzara@gmail.com>
2026-01-30 10:06:32 +0100

    définition des topics

---
+++ esp32/station.ino
@@ -0,0 +1,4 @@
+const char* TOPIC_TEMPERATURE = "station-meteo/temperature";
+const char* TOPIC_HUMIDITY    = "station-meteo/humidity";
+const char* TOPIC_UNIT        = "station-meteo/unit";
+const char* TOPIC_SET_UNIT    = "station-meteo/set-unit";
