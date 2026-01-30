8ac531d2540ac7278c2d7e30a7a30e8dbcdc2185
NotUndefind <julesbourin@icloud.com>
2026-01-30 14:47:53 +0100

    update station.ino for real data

--- esp32/station.ino
+++ esp32/station.ino
@@ -1,6 +1,7 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include "DHT.h";
 
 // WiFi
 const char *WIFI_SSID = "Galyst";
@@ -19,6 +20,26 @@ const char *TOPIC_SET_UNIT = "station-meteo/set-unit";
 unsigned long lastPublishTime = 0;
 const unsigned long PUBLISH_INTERVAL = 1000;
 
+// DHT Sensor
+#define DHTPIN 17
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// Pins
+const int LED_PIN_C = 13;
+const int LED_PIN_F = 14;
+const int BUTTON_PIN = 16;
+
+// Simulation Variables
+float tempCelsius = 20.0;
+float humidity = 50.0;
+bool isCelsius = true;
+bool lastButtonState = HIGH;
+// Timing for simulation
+unsigned long previousMillisData = 0;
+const long intervalData = 1000;
+
+// WiFi and MQTT Clients
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
@@ -60,7 +81,7 @@ void connectMQTT() {
 String createDataJson(float temperature, float humidity, char unit) {
   StaticJsonDocument<200> doc;
 
-  doc["id"] = "12";
+  doc["id"] = "esp32-01";
   doc["temperature"] = temperature;
   doc["humidity"] = humidity;
   doc["unit"] = String(unit);
@@ -92,8 +113,17 @@ void publishWeatherData(float temperature, float humidity, char unit) {
 
 // Setup
 void setup() {
+    // Initialize Serial
   Serial.begin(115200);
-  delay(1000);
+  // Initialize pins
+  pinMode(LED_PIN_C, OUTPUT);
+  pinMode(LED_PIN_F, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  // Initialize DHT sensor
+  dht.begin();
+  delay(200);
+  // Initialize button state
+  lastButtonState = digitalRead(BUTTON_PIN);
 
   Serial.println("\n=== ESP32 Weather Station Starting ===");
 
@@ -125,4 +155,48 @@ void loop() {
 
     lastPublishTime = currentTime;
   }
+
+    unsigned long currentMillis = millis();
+    bool currentButtonState = digitalRead(BUTTON_PIN);
+
+    if (currentButtonState != lastButtonState && currentButtonState == LOW){
+      isCelsius = !isCelsius;
+      
+      if (isCelsius) {
+        digitalWrite(LED_PIN_C, HIGH);
+        digitalWrite(LED_PIN_F, LOW);
+      } else {
+        digitalWrite(LED_PIN_C, LOW);
+        digitalWrite(LED_PIN_F, HIGH);
+      }
+      
+      delay(50); 
+    }
+    lastButtonState = currentButtonState;
+
+    if (currentMillis - previousMillisData >= intervalData) {
+      tempCelsius += random(-5, 6) / 10.0;
+      humidity += random(-10, 11) / 10.0;
+
+      if (tempCelsius > 40.0) tempCelsius = 40.0;
+      if (tempCelsius < 10.0) tempCelsius = 10.0;
+      if (humidity > 100.0) humidity = 100.0;
+      if (humidity < 0.0) humidity = 0.0;
+
+      if (isCelsius) {
+        publishWeatherData(tempCelsius, humidity, 'C');
+      } else {
+        float tempFahrenheit = (tempCelsius * 1.8) + 32.0;
+        publishWeatherData(tempFahrenheit, humidity, 'F');
+              Serial.print("Temp: ");
+      Serial.print(tempF);
+      Serial.print(" °F");
+      }
+
+      Serial.print(" | Hum: ");
+      Serial.print(humidity);
+      Serial.println(" %");
+
+      previousMillisData = currentMillis;
+    }
 }
