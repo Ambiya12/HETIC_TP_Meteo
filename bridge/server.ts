68d5b1bab49aee6c70d91048e8d2c82be7fb16eb
NotUndefind <julesbourin@icloud.com>
2026-01-30 11:31:00 +0100

    ajout du bridge

---
+++ bridge/server.ts
@@ -0,0 +1,44 @@
+const Host = "captain.dev0.pandor.cloud";
+const Port = "1884";
+
+import * as WebSocket from "ws";
+import * as Mqtt from "mqtt";
+
+const wsServer = new WebSocket.WebSocketServer({ port: 8080 });
+const client = Mqtt.connect("mqtt://" + Host + ":" + Port);
+
+const webSocketClient: WebSocket[] = [];
+const mqttTopics = [
+	"station-meteo/temperature",
+	"station-meteo/humidity",
+	"station-meteo/unit",
+	"station-meteo/set-unit",
+];
+
+console.log("Starting Bridge Server...");
+
+client.on("connect", () => {
+	client.subscribe(mqttTopics, (err: Error | null): void => {
+		if (!err) {
+			console.log("Connected to MQTT Broker and subscribed to topics.");
+		} else {
+			console.error("Failed to subscribe to MQTT topics:", err);
+		}
+	});
+});
+
+client.on("message", (topic: string, message: Buffer) => {
+	const payload = {
+		topic: topic,
+		message: JSON.parse(message.toString()),
+	};
+
+	webSocketClient.forEach((ws: WebSocket): void => {
+		ws.send(JSON.stringify(payload));
+	});
+});
+
+wsServer.on("connection", (ws: WebSocket) => {
+	webSocketClient.push(ws);
+	console.log("New WebSocket client connected.");
+});
