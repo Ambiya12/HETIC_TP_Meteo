f4ca0ea971cf07bacc3023f627dc180d606dc030
antoine <antoinetu2003@gmail.com>
2026-01-30 10:14:51 +0100

    Format des messages : JSON

--- esp32/station.ino
+++ esp32/station.ino
@@ -1,4 +1,13 @@
-const char* TOPIC_TEMPERATURE = "station-meteo/temperature";
-const char* TOPIC_HUMIDITY    = "station-meteo/humidity";
-const char* TOPIC_UNIT        = "station-meteo/unit";
-const char* TOPIC_SET_UNIT    = "station-meteo/set-unit";
+const char *TOPIC_TEMPERATURE = "station-meteo/temperature";
+const char *TOPIC_HUMIDITY = "station-meteo/humidity";
+const char *TOPIC_UNIT = "station-meteo/unit";
+const char *TOPIC_SET_UNIT = "station-meteo/set-unit";
+
+{
+  "id" : "station_groupe_12",
+  "temperature" : 24.5,
+  "humidity" : 60,
+  "unit" : "C"
+}
+
+{"action" : "set_unit", "value" : "F"}
